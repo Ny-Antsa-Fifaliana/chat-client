694d757ab6242380ab57903346456de2257b4ef9
Fifa <nyantsafifaliana240@gmail.com>
2024-11-09 05:20:20 +0300

    no encoded url client

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,129 +1,141 @@
-import React, {useState, useEffect} from 'react';
-import queryString from 'query-string';
-import io from 'socket.io-client';
-import {useLocation} from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
+import { useLocation } from "react-router-dom";
 
-import './Chat.css';
-import InfoBar from '../InfoBar/InfoBar';
-import Input from '../Input/Input';
-import Messages from '../Messages/Messages';
-import Stack from '@mui/material/Stack';
+import "./Chat.css";
+import InfoBar from "../InfoBar/InfoBar";
+import Input from "../Input/Input";
+import Messages from "../Messages/Messages";
+import Stack from "@mui/material/Stack";
 
- let socket;
+let socket;
 
-
- 
-const notificationSoundReceive = new Audio('/sounds/ping.mp3');
-const notificationSoundSend = new Audio('/sounds/send.mp3');
-const notificationSoundAdmin = new Audio('/sounds/admin.mp3');
-
+const notificationSoundReceive = new Audio("/sounds/ping.mp3");
+const notificationSoundSend = new Audio("/sounds/send.mp3");
+const notificationSoundAdmin = new Audio("/sounds/admin.mp3");
 
 const playNotificationSoundReceive = () => {
-    notificationSoundReceive.play();
+  notificationSoundReceive.play();
 };
 
 const playNotificationSoundSend = () => {
-    notificationSoundSend.play();
+  notificationSoundSend.play();
 };
-const playNotificationSoundAdmin= () => {
-    notificationSoundAdmin.play();
+const playNotificationSoundAdmin = () => {
+  notificationSoundAdmin.play();
 };
 
+notificationSoundReceive.muted = true;
+const Chat = () => {
+  //variable
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState("");
+  const [usersInRoom, setUsersInRoom] = useState([]);
+  const [showInput, setShowInput] = useState(true);
 
+  const ENDPOINT = "https://torch-tartan-gouda.glitch.me/"; //server url
+  //   const ENDPOINT = "localhost:5000"; //server url
 
+  const location = useLocation(); // Get current URL
+  //   const urlDecoded = decodeURIComponent(location.search);
+  const urlDecoded = location.search;
 
+  //initialisation
+  useEffect(() => {
+    const { name, room } = queryString.parse(urlDecoded); // this return object json name and room
+    setName(name);
+    setRoom(room);
+    socket = io(ENDPOINT); // connection au serveur
+    socket.emit("joindre", { name, room }); //First emit
 
-notificationSoundReceive.muted=true;
-const Chat = ()=>{
-    //variable
-    const [name, setName] = useState('');
-    const [room, setRoom] = useState('');
-    const [messages, setMessages] = useState([]);
-    const [message, setMessage] = useState('');
-    const [usersInRoom, setUsersInRoom] = useState([]);
-    const [showInput, setShowInput] = useState(true);
+    return () => {
+      socket.disconnect();
+    };
+  }, [ENDPOINT, urlDecoded]);
 
-    const ENDPOINT = 'https://torch-tartan-gouda.glitch.me/'; //server url 
-    const location = useLocation(); // Get current URL 
-    const urlDecoded = decodeURIComponent(location.search)
+  // after connected
+  useEffect(() => {
+    socket.on(
+      "message",
+      (message) => {
+        setMessages([...messages, message]);
+      },
+      () => {}
+    );
+    socket.on(
+      "roomData",
+      (data) => {
+        setUsersInRoom(data);
+      },
+      () => {}
+    );
+  }, [messages]);
 
+  // Notification
+  // Jouer le son uniquement lorsque le dernier message est envoyé par l'utilisateur actuel
+  useEffect(() => {
+    if (messages.length > 0 && messages[messages.length - 1].user) {
+      if (
+        messages[messages.length - 1].user.charAt(0).toUpperCase() +
+          messages[messages.length - 1].user.slice(1).toLowerCase() ===
+        name.charAt(0).toUpperCase() + name.slice(1).toLowerCase()
+      ) {
+        notificationSoundReceive.muted = false;
+        playNotificationSoundSend();
+      } else if (
+        messages[messages.length - 1].user.charAt(0).toUpperCase() +
+          messages[messages.length - 1].user.slice(1).toLowerCase() ===
+          "Admin" &&
+        !notificationSoundReceive.muted
+      ) {
+        playNotificationSoundAdmin();
+      } else if (notificationSoundReceive.muted) {
+        window.addEventListener("click", () => {
+          notificationSoundReceive.muted = false;
+        });
+      } else {
+        playNotificationSoundReceive();
+      }
+    }
+    return window.removeEventListener("click", () => {
+      notificationSoundReceive.muted = false;
+    });
+  }, [messages, name]);
 
-        //initialisation
-        useEffect(()=>{
-             const {name,room} = queryString.parse(urlDecoded); // this return object json name and room
-             setName(name);
-             setRoom(room);
-             socket = io(ENDPOINT); // connection au serveur
-             socket.emit('joindre',{name,room}); //First emit
+  //function to sendMessage
+  const sendMessage = (e) => {
+    e.preventDefault();
+    if (message) {
+      socket.emit("sendMessage", message, () => setMessage(""));
+    }
+  };
 
-             return()=>{
-                socket.disconnect();
-             }
-        },[ENDPOINT,urlDecoded]);
+  const handleTabChange = (tab) => {
+    setShowInput(tab === "discussion");
+  };
 
-
-        // after connected
-        useEffect(()=>{
-            socket.on('message',(message)=>{
-                setMessages([...messages, message]);
-            }, ()=>{});
-            socket.on('roomData',(data)=>{
-                setUsersInRoom(data)
-            }, ()=>{});
-        },[messages]);
-
-
-
-
-        // Notification
-        // Jouer le son uniquement lorsque le dernier message est envoyé par l'utilisateur actuel
-        useEffect(() => {
-            if (messages.length > 0 && messages[messages.length - 1].user) {
-                if(messages[messages.length - 1].user.charAt(0).toUpperCase()+messages[messages.length - 1].user.slice(1).toLowerCase()===name.charAt(0).toUpperCase()+name.slice(1).toLowerCase()){
-                    notificationSoundReceive.muted=false;
-                    playNotificationSoundSend();
-                }
-                else if(messages[messages.length - 1].user.charAt(0).toUpperCase()+messages[messages.length - 1].user.slice(1).toLowerCase() === 'Admin' && !notificationSoundReceive.muted){
-                    playNotificationSoundAdmin();
-                }
-
-                else if (notificationSoundReceive.muted){
-                    window.addEventListener('click',()=>{notificationSoundReceive.muted=false});
-                }
-                else{
-                    playNotificationSoundReceive();
-                }
-               
-            
-            }
-            return(
-                window.removeEventListener('click',()=>{notificationSoundReceive.muted=false})
-            )
-        }, [messages, name]);
-
-
-
-
-        //function to sendMessage
-        const sendMessage = (e)=>{
-            e.preventDefault();
-            if(message){
-                socket.emit('sendMessage', message, ()=>setMessage(''));
-            }
-        }    
-
-        const handleTabChange = (tab) => {
-            setShowInput(tab === 'discussion');
-        };
-
-    // Affichage
-    return (
-        <Stack className='conteneurChat' direction="column">
-            <InfoBar room={room}/>
-            <Messages messages={messages} name={name} usersInRoom={usersInRoom}  handleTabChange={handleTabChange} />
-            {showInput && <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />}  
-        </Stack>
-    )   
+  // Affichage
+  return (
+    <Stack className="conteneurChat" direction="column">
+      <InfoBar room={room} />
+      <Messages
+        messages={messages}
+        name={name}
+        usersInRoom={usersInRoom}
+        handleTabChange={handleTabChange}
+      />
+      {showInput && (
+        <Input
+          message={message}
+          setMessage={setMessage}
+          sendMessage={sendMessage}
+        />
+      )}
+    </Stack>
+  );
 };
 
 export default Chat;
--- src/components/Join/Join.js
+++ src/components/Join/Join.js
@@ -1,44 +1,50 @@
-import React, {useState} from 'react';
-import { Link } from 'react-router-dom';
-import TextField from '@mui/material/TextField';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 
+import "./Join.css";
 
-import './Join.css'
+const Join = () => {
+  //variable
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
 
+  //function
 
-const Join = ()=>{
+  //Affichage
+  return (
+    <Stack spacing={6} className="outerContainer">
+      <Stack spacing={3} className="container">
+        <p className="echat">inchat</p>
 
-    //variable
-    const [name, setName] = useState('');
-    const [room, setRoom] = useState('');
-
-
-    //Affichage
-    return (
-        <Stack spacing={6} className='outerContainer'>
-            <Stack spacing={3} className='container'>
-                <p className='echat'>inchat</p>
-               
-                <TextField
-                    helperText="Entrez votre pseudo."
-                    id="nameId"
-                    label="Pseudo"
-                    onChange={(e)=>setName(e.target.value)}
-                />
-                <TextField
-                    helperText="Créer ou rejoindre une discussion."
-                    id="roomId"
-                    label="Nom de la discussion..."
-                    onChange={(e)=>setRoom(e.target.value)} 
-                />
-                <Link style={{textDecoration:'none'}} className='btn__join' onClick={e=>(!name || !room)? e.preventDefault() : null} to={'/chat?'+encodeURIComponent('name='+name+'&room='+room)}>
-                    <Button fullWidth type='submit' variant="contained">Rejoindre</Button>
-                </Link>
-            </Stack>
-        </Stack>
-    )
+        <TextField
+          helperText="Entrez votre pseudo."
+          id="nameId"
+          label="Pseudo"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <TextField
+          helperText="Créer ou rejoindre une discussion."
+          id="roomId"
+          label="Nom de la discussion..."
+          onChange={(e) => setRoom(e.target.value)}
+        />
+        {/* <Link style={{textDecoration:'none'}} className='btn__join' onClick={e=>(!name || !room)? e.preventDefault() : null} to={'/chat?'+encodeURIComponent('name='+name+'&room='+room)}> */}
+        <Link
+          style={{ textDecoration: "none" }}
+          className="btn__join"
+          onClick={(e) => (!name || !room ? e.preventDefault() : null)}
+          to={"/chat?name=" + name + "&room=" + room}
+        >
+          <Button fullWidth type="submit" variant="contained">
+            Rejoindre
+          </Button>
+        </Link>
+      </Stack>
+    </Stack>
+  );
 };
 
 export default Join;
