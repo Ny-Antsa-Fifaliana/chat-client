db868e0588fafdeca64646e999799c671af157ed
Fifa <nyantsafifaliana240@gmail.com>
2024-11-18 04:46:23 +0300

    change server after local socket

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -37,7 +37,7 @@ const Chat = () => {
   const [showInput, setShowInput] = useState(true);
   const [files, setFiles] = useState([]);
 
-  const ENDPOINT = "https://woolly-horn-tarsier.glitch.me"; //server url
+  const ENDPOINT = "https://evanescent-understood-fisherman.glitch.me"; //server url
   //const ENDPOINT = "http://localhost:5000"; //server url
 
   const location = useLocation(); // Get current URL
